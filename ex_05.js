bf673988632a61978ca57f63d30d9e7da89d73d8
Marie Caron <marie.caron@epitech.eu>
2025-02-24 15:26:34 +0100

    feat: creation variable that diplay other variables about the credit card.

---
+++ ex_05.js
@@ -0,0 +1,8 @@
+const firstname = "John"
+const lastname = "Doe"
+const cardCode = "1111 2222 3333 4444"
+const expiryDateMonth = 21
+const expiryDateYear = 2042
+const computedBankInfo = `${firstname}, ${lastname}, ${cardCode}, ${expiryDateMonth}/${expiryDateYear}`
+
+displayInCard(computedBankInfo)
