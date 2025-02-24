d4b57858609eb6dd6b4f967e5d69ed7e1edaf89c
Marie Caron <marie.caron@epitech.eu>
2025-02-24 16:57:14 +0100

    fix: space conditions correction.

--- ex_09.js
+++ ex_09.js
@@ -2,34 +2,34 @@ if (values[0] == 42) {
 	displayThisText("The variable value is '" + values[0] + "'" + ".")
 	displayThisText("Its type is 'String'.")
     displayThisText("It is the meaning of life.")
-	} else {
-  displayThisText("The variable value is '" + values[0] + "'" + ".")
+} else {
+    displayThisText("The variable value is '" + values[0] + "'" + ".")
 	displayThisText("Its type is 'String'.")
-  }
+}
 
 if (values[3] == 42) {
 	displayThisText("The variable value is '" + values[3] + "'" + ".")
 	displayThisText("Its type is 'Boolean'.")
-   displayThisText("It is the meaning of life.")
-  } else {
-  displayThisText("The variable value is '" + values[3] + "'" + ".")
+    displayThisText("It is the meaning of life.")
+} else {
+    displayThisText("The variable value is '" + values[3] + "'" + ".")
 	displayThisText("Its type is 'Boolean'.")
-  }
+}
 
 if (values[4] == 42) {
 	displayThisText("The variable value is '" + values[4] + "'" + ".")
 	displayThisText("Its type is 'Integer'.")
-   displayThisText("It is the meaning of life.")
-  } else {
-  displayThisText("The variable value is '" + values[4] + "'" + ".")
+    displayThisText("It is the meaning of life.")
+} else {
+    displayThisText("The variable value is '" + values[4] + "'" + ".")
 	displayThisText("Its type is 'Integer'.")
-  }
+}
 
 if (values[1] == 42) {
 	displayThisText("The variable value is '" + values[1] + "'" + ".")
 	displayThisText("Its type is 'Float'.")
-  displayThisText("It is the meaning of life.")
-  } else {
-  displayThisText("The variable value is '" + values[1] + "'" + ".")
+    displayThisText("It is the meaning of life.")
+} else {
+    displayThisText("The variable value is '" + values[1] + "'" + ".")
 	displayThisText("Its type is 'Float'.")
-  }
+}
