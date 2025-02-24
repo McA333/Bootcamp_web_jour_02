8c3cde327c62d493691573c6069d65a91744774d
Marie Caron <marie.caron@epitech.eu>
2025-02-24 11:58:39 +0100

    feat: creation 5 new variables and display.

---
+++ ex_02.js
@@ -0,0 +1,11 @@
+const integer = 42
+const float = 42.42
+const string = "forty two"
+const bool = true
+const this_is_null = null
+
+displayThisText(bool)
+displayThisText(string)
+displayThisText(integer)
+displayThisText(this_is_null)
+displayThisText(float)
