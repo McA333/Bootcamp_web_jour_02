43e47272c4e44ce1ab4ae78d0b317acea79bb35e
Marie Caron <marie.caron@epitech.eu>
2025-02-24 12:29:08 +0100

    feat: different ways of using typeof.

---
+++ ex_04.js
@@ -0,0 +1,7 @@
+displayThisText(typeof undefined)
+displayThisText(typeof true)
+displayThisText(typeof 42)
+displayThisText(typeof BigInt(999999999999999))
+displayThisText(typeof Symbol(":"))
+displayThisText(typeof null)
+displayThisText(typeof Object)
